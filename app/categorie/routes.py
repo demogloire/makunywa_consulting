f0c3d2e0548f52b41a71d756af08de105fbccb19
MUSA <chirimwami2014@gmail.com>
2020-10-18 13:47:34 +0200

    Ajout de la categorie

--- app/categorie/routes.py
+++ app/categorie/routes.py
@@ -3,13 +3,14 @@ from .. import db, bcrypt
 from ..models import Categorie
 from app.categorie.forms import AjoutCatForm
 from flask_login import login_user, current_user, logout_user, login_required
+from ..utilites.utility import title_page
 from . import categorie
 
 
 
 ''' Ajoute une categorisation '''
 
-@categorie.route('/ajouter/categorie', methods=['GET','POST'])
+@categorie.route('/ajouter', methods=['GET','POST'])
 def ajoutcate():
    title='Catégorie | Makunywa Consulting'
    #formulaire
@@ -20,85 +21,73 @@ def ajoutcate():
       categorie_enre=Categorie(nom=nom_cat)
       db.session.add(categorie_enre)
       db.session.commit()
-      flash("Ajout avec succès une nouvelle categorisation",'success')
-      return redirect(url_for('categorie.litcate')) 
+      flash("Ajout avec succès",'primry')
+      return redirect(url_for('categorie.index')) 
 
    return render_template('categorie/ajouter.html',  title=title, form=form)
 
 """ Liste des types """
 
-@categorie.route('/liste/categorie', methods=['GET', 'POST'])
+@categorie.route('/liste', methods=['GET', 'POST'])
 def index():
    #Titre
-   title='Catégorie | Makunywa Consulting'
+   title=title_page('Catégorie')
    #Requête d'affichage de la categorisation
-   listes=Categorie.query.order_by(Categorie.id.desc())
+   listes=Categorie.query.order_by(Categorie.id.desc()).all()
+   
 
    return render_template('categorie/index.html',title=title, liste=listes)
 
 # """ Modifier statut de l'Utilisateur """
 
-# @categorie.route('/statut_cate/<int:cat_id>', methods=['GET', 'POST'])
-# @login_required
-# def statutcat(cat_id):
-#    #Titre
-#    title='Categorisation | Kivu Exchange'
+@categorie.route('/statut/<int:cat_id>', methods=['GET', 'POST'])
+def statut(cat_id):
+   #Titre
+   title=title_page('Catégorie')
+   #Requête de vérification de la categorie
+   cat_statu=Categorie.query.filter_by(id=cat_id).first()
 
-#    #La modification du mot de passe par l'administrateur.
-#    if current_user.role!='Admin':
-#       return redirect(url_for('main.dashboard'))
+   if cat_statu is None:
+      return redirect(url_for('categorie.index'))
 
-#    #Requête de vérification de la categorie
-#    cat_statu=Categorie.query.filter_by(id=cat_id).first()
-
-#    if cat_statu is None:
-#       return redirect(url_for('categorie.litcate'))
-
-#    if cat_statu.statut == True:
-#       cat_statu.statut=False
-#       db.session.commit()
-#       flash("La catégorie est désactivée sur la plateforme",'success')
-#       return redirect(url_for('categorie.litcate'))
-#    else:
-#       cat_statu.statut=True
-#       db.session.commit()
-#       flash("La catégorie est activée sur la plateforme",'success')
-#       return redirect(url_for('categorie.litcate'))
+   if cat_statu.statut == True:
+      cat_statu.statut=False
+      db.session.commit()
+      flash("La catégorie est désactivée sur la plateforme",'primary')
+      return redirect(url_for('categorie.index'))
+   else:
+      cat_statu.statut=True
+      db.session.commit()
+      flash("La catégorie est activée sur la plateforme",'primary')
+      return redirect(url_for('categorie.index'))
    
-#    return render_template('user/views.html',title=title)
+   return render_template('user/index.html',title=title)
 
 
 # """ Modification de la catégorie  """
 
-# @categorie.route('/edit_<int:cate_id>_cate', methods=['GET', 'POST'])
-# @login_required
-# def editcate(cate_id):
-       
-#    #La modification du mot de passe par l'administrateur.
-#    if current_user.role!='Admin':
-#       return redirect(url_for('main.dashboard'))       
-   
-#    form=EditCatForm()
-#    #Titre
-#    title='Catégorisation | Kivu Exchange'
-#    #Requête de vérification du type
-#    cate_class=Categorie.query.filter_by(id=cate_id).first()
-#    #Le nom du type encours de modification
-#    cate_nom=cate_class.nom
+@categorie.route('/edit_<int:cate_id>_cate', methods=['GET', 'POST'])
+def edit(cate_id):
 
-#    if cate_class is None:
-#       return redirect(url_for('categorie.litcate'))
+   form=AjoutCatForm()
+   #Titre
+   title=title_page('Catégorie')
+   #Requête de vérification du type
+   cate_class=Categorie.query.filter_by(id=cate_id).first()
+   #Le nom du type encours de modification
+   cate_nom=cate_class.nom
+
+   if cate_class is None:
+      return redirect(url_for('categorie.index'))
    
-#    if form.validate_on_submit(): 
-#       cate_class.nom=form.ed_nom.data.capitalize()
-#       cate_class.type_id=form.ed_rech_type.data.id
-#       db.session.commit()
-#       flash("Modification réussie",'success')
-#       return redirect(url_for('categorie.litcate'))
+   if form.validate_on_submit(): 
+      cate_class.nom=form.nom.data.capitalize()
+      db.session.commit()
+      flash("Modification réussie",'primary')
+      return redirect(url_for('categorie.index'))
       
-#    if request.method=='GET':
-#       form.ed_nom.data=cate_class.nom
-#       form.ed_rech_type.data=cate_class.type_categorie.nom
+   if request.method=='GET':
+      form.nom.data=cate_class.nom
 
-#    return render_template('categorie/editcat.html', form=form, title=title, cate_nom=cate_nom)
+   return render_template('categorie/edit.html', form=form, title=title, cate_nom=cate_nom)
 
---
+++ app/utilites/utility.py
@@ -0,0 +1,5 @@
+#Fonction de title
+
+def title_page(nom="Dashbord"):
+    title=f'{nom} | Makunywa Consulting'
+    return title
